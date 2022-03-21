40136c42d2037bbd71962e0928feb52eda986f43
BonnyWang <bonny.y.gardy@gmail.com>
2022-03-20 23:58:29 -0400

    Added halos

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Generator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    string Halo_Path = @"O:\UnityProj\Voids-VR-Scene_Unity\Assets\Data\halos_In_100.dat";
+    public GameObject halo_Prefab;
     void Start()
     {
-        
+        using(var reader = new StreamReader(Halo_Path))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                
+            }
+        }
     }
 
     // Update is called once per frame
