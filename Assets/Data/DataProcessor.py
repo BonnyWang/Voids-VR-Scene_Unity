e21f4cc8c061fe7b7e808ab5372e961450ba5148
BonnyWang <bonny.y.gardy@gmail.com>
2022-03-21 23:18:02 -0400

    Added Voids in 100 range

--- Assets/Data/DataProcessor.py
+++ Assets/Data/DataProcessor.py
@@ -1,8 +1,7 @@
 import pandas as pd;
-import os;
 
 Halo_Path = r"O:\UnityProj\Voids-VR-Scene_Unity\Assets\Data\mfiducial_663_halos.dat";
-Void_Path = "./centers_all_Quijote_fiducial_663_RSD_ss1.0_z0.00_d00.out";
+Void_Path = r"O:\UnityProj\Voids-VR-Scene_Unity\Assets\Data\centers_all_Quijote_fiducial_663_RSD_ss1.0_z0.00_d00.out";
 
 def selectHalos(xLimit, yLimit, zLimit, outPath):
     halo_Data = pd.read_csv(Halo_Path, sep=" ");
@@ -12,7 +11,18 @@ def selectHalos(xLimit, yLimit, zLimit, outPath):
     halo_InRange = halo_InRange[["x","y","z"]];
     
     halo_InRange.to_csv(outPath, index=False, header=False);
+
+def selectVoids(xLimit, yLimit, zLimit, outPath):
+    void_Data = pd.read_csv(Void_Path, sep=" ");
+    
+    void_InRange = void_Data.loc[(void_Data["x"] < xLimit) & (void_Data["y"] < yLimit)& (void_Data["z"] < zLimit)];
+    
+    void_InRange = void_InRange[["x","y","z", "radius(Mpc/h)"]];
+    
+    void_InRange.to_csv(outPath, index=False, header=False);
+    
     
 
 if __name__ == "__main__":
     selectHalos(100,100,100,"./halos_In_100.dat");
+    selectVoids(100,100,100,"./voids_In_100.dat");
--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -7,9 +7,13 @@ using UnityEditor;
 public class Generator : MonoBehaviour
 {
     string Halo_Path = @"O:\UnityProj\Voids-VR-Scene_Unity\Assets\Data\halos_In_100.dat";
+    string Void_Path = @"O:\UnityProj\Voids-VR-Scene_Unity\Assets\Data\voids_In_100.dat";
     public GameObject halo_Prefab;
+    public GameObject void_Prefab;
     public GameObject halo_Parent;
+    public GameObject void_Parent;
     GameObject tempHalo;
+    GameObject tempVoid;
     void Start()
     {
         using(var reader = new StreamReader(Halo_Path))
@@ -19,13 +23,32 @@ public class Generator : MonoBehaviour
                 var line = reader.ReadLine();
                 var values = line.Split(',');
 
-                 tempHalo = Instantiate(halo_Prefab, new Vector3(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2])),Quaternion.identity);
+                tempHalo = Instantiate(halo_Prefab, new Vector3(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2])),Quaternion.identity);
 
-                 tempHalo.transform.parent = halo_Parent.transform;
+                tempHalo.transform.parent = halo_Parent.transform;
                 
             }
 
             PrefabUtility.SaveAsPrefabAsset(halo_Parent, "Assets/Prefabs/Generated_Halos.prefab");
+        }
+
+        using(var reader = new StreamReader(Void_Path))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                tempVoid = Instantiate(void_Prefab, new Vector3(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2])),Quaternion.identity);
+                
+                float voidScale = float.Parse(values[3]);
+                tempVoid.transform.localScale = new Vector3(voidScale, voidScale, voidScale);
+
+                tempVoid.transform.parent = void_Parent.transform;
+                
+            }
+
+            PrefabUtility.SaveAsPrefabAsset(halo_Parent, "Assets/Prefabs/Generated_Voids.prefab");
         }
     }
 
